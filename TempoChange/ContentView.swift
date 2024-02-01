1e886fc72f00ba372c7be4dcbd52f7006215ab8e
Alexis Doualle <adoualle@gmail.com>
2024-02-01 15:42:05 +0100

    Initial Commit

---
+++ TempoChange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TempoChange
+//
+//  Created by Alexis Doualle on 01/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TempoChange/TempoChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoChangeApp.swift
+//  TempoChange
+//
+//  Created by Alexis Doualle on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TempoChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
